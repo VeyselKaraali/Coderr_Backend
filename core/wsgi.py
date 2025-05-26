8253a3e684ccd87f7cbe65a83518048391548614
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-05-26 15:27:54 +0200

    Delete old project and start new Django project 'core'

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Coderr_Backend project.
+WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coderr_Backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
