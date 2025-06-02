521c351591ed6674d95e34e4f06deb99ef6a6788
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-06-02 13:19:47 +0200

    Implement user, authentication, and profile models with admin setup

---
+++ app_authentication/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from app_authentication.models import Account
+
+
+@admin.register(Account)
+class AccountAdmin(UserAdmin):
+    readonly_fields = ['created_at', 'updated_at', 'last_login']
+    list_display = ['username', 'email', 'type', 'is_active', 'is_staff', 'is_guest']
+    list_filter = ['type', 'is_active', 'is_staff', 'is_guest']
+    search_fields = ['username', 'email']
+    ordering = ['username', 'created_at']
+
+    personal_info_fields = ('email', 'type', 'is_guest')
+    permissions_fields = ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+    important_dates_fields = ('created_at', 'updated_at', 'last_login')
+
+    add_fieldsets = (
+        (None, {'classes': ('wide',), 'fields': ('username', 'password1', 'password2')}),
+        ('Personal info', {'classes': ('wide',), 'fields': personal_info_fields}),
+        ('Permissions', {'classes': ('wide',), 'fields': permissions_fields}),
+    )
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': personal_info_fields}),
+        ('Permissions', {'fields': permissions_fields}),
+        ('Important dates', {'fields': important_dates_fields}),
+    )
--- app_profile/models.py
+++ app_profile/models.py
@@ -1,32 +1,15 @@
-from django.core.exceptions import ValidationError
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from app_authentication.models import Account, AccountType
+from core.models import TimeStamped
+from django.db.models import Q
 
-
-class ProfileManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
-        if not username:
-            raise ValueError('The Username field must be set.')
-        user = self.model(username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-        return self.create_user(username, password, **extra_fields)
-
-
-class ProfileType(models.TextChoices):
-    CUSTOMER = 'CUSTOMER', 'Customer'
-    BUSINESS = 'BUSINESS', 'BUSINESS'
-
-
-class Profile(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField (max_length=32, unique=True, null=False, blank=False)
-    email = models.EmailField(unique=True, null=True, blank=True)
+class Profile(TimeStamped):
+    account = models.OneToOneField(
+        Account,
+        on_delete=models.CASCADE,
+        limit_choices_to=~Q(type=AccountType.SUPERUSER),
+        related_name='profile'
+    )
     first_name = models.CharField(max_length=32, blank=True, default='')
     last_name = models.CharField(max_length=32, blank=True, default='')
     file = models.FileField(null=True, blank=True)
@@ -34,25 +17,7 @@ class Profile(AbstractBaseUser, PermissionsMixin):
     tel = models.CharField(max_length=32, blank=True, default='')
     description = models.TextField(blank=True, default='')
     working_hours = models.CharField(max_length=64, blank=True, default='')
-    type = models.CharField(max_length=16, choices=ProfileType.choices)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_guest = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    objects = ProfileManager()
-
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'type']
-
-    def clean(self):
-        super().clean()
-        if not self.is_guest and not self.email:
-            raise ValidationError({'email': 'Email is required'})
-
-    def save(self, *args, **kwargs):
-        self.full_clean()
-        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.username}"
+        return f"{self.account.username}"
+
