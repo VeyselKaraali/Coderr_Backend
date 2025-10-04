edf7f39d1de3550502270e2ff0f2d855577987aa
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-10-05 01:46:17 +0200

    Register Offer, Order and Review model in Django admin

--- app_offers/admin.py
+++ app_offers/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_offers.models import Offer
+
+
+admin.site.register(Offer)
--- app_reviews/admin.py
+++ app_reviews/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_reviews.models import Review
+
+
+admin.site.register(Review)
