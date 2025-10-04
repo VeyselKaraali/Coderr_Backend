ab58e019a141d91c59fc6eada375b7f8b19cb85c
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-10-05 01:32:18 +0200

    Add IsReviewerOrReadOnly permission class

--- app_authentication/permissions.py
+++ app_authentication/permissions.py
@@ -18,3 +18,10 @@ class IsProfileOwnerOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return obj.user == request.user
+
+
+class IsReviewerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.reviewer == request.user
